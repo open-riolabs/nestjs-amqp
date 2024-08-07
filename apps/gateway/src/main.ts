6250421220d59c011d811d7d5e4f90e9264a73fe
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2024-08-07 17:02:43 +0200

    fix

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -7,6 +7,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appConfig = app.get<ConfigService>(ConfigService).get<AppConfig>('app');
-  await app.listen(appConfig.port + 1, appConfig.host);
+  await app.listen(appConfig.port, appConfig.host);
 }
 bootstrap();
