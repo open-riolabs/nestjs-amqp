c62dc2017194f03ad29ef03a998923b021512edf
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2024-10-21 23:45:17 +0200

    added file test

--- apps/gateway/src/app.service.ts
+++ apps/gateway/src/app.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { BrokerEvent, BrokerService } from '@rlb/nestjs-amqp';
 import { NotFoundError } from '@rlb/nestjs-core';
+import { writeFile } from 'fs/promises'
 
 @Injectable()
 export class AppService implements OnModuleInit {
@@ -8,13 +9,17 @@ export class AppService implements OnModuleInit {
   async onModuleInit() {
     setInterval(async () => { }, 1000);
 
-    // await
-    //   this.brokerService.registerRpc('local-test', async (data) => {
-    //     switch (data.payload.action) {
-    //       case 'test-01': return { data: 'Hello from RPC' };
-    //       case 'test-02': throw new NotFoundError('Not found');
-    //     }
-    //   });
+    await
+      this.brokerService.registerRpc('local-test', async (data) => {
+        switch (data.payload.action) {
+          case 'test-01': {
+            const o  = Buffer.from(data.payload.payload.$files[0].buffer, 'binary')
+            await writeFile('test.pdf', o)
+            return { data: 'Hello from RPC' };
+          }
+          case 'test-02': throw new NotFoundError('Not found');
+        }
+      });
     this.brokerService.getEvents$<any>().subscribe((event: BrokerEvent) => {
       console.log(event);
     });
