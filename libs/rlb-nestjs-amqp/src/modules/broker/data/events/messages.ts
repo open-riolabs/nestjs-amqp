fe916d75a626a48d0e273f38c8668fe950251c0c
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2025-03-05 16:29:47 +0100

    Enhance BrokerEvent and RpcEvent structures to include action property and update message publishing logic

--- libs/rlb-nestjs-amqp/src/modules/broker/data/events/messages.ts
+++ libs/rlb-nestjs-amqp/src/modules/broker/data/events/messages.ts
@@ -3,17 +3,18 @@ import { Observable } from "rxjs";
 export interface BrokerEvent<Payload = any> {
   topic: string;
   payload: Payload;
+  action: string;
   source: {
     exchange: string;
     routingKey: string;
     tag: string;
-  }
-  headers: { [key: string]: any };
+  };
+  headers: { [key: string]: any; };
   raw: Buffer;
 }
 
-export type RpcEvent<Payload = any> = { action: string; payload: Payload; }
+export type RpcEvent<Payload = any> = { action: string; payload: Payload; };
 export type TopicEventHandler<Payload = any> = (event: BrokerEvent<Payload>) => void | Promise<void> | Observable<void>;
 export type BrokerEventHandler<Payload = any, Response = any> = (event: BrokerEvent<Payload>) => Response | Promise<Response> | Observable<Response>;
 export type RpcEventHandler<Payload = any, Response = any> = (event: BrokerEvent<RpcEvent<Payload>>) => Response | Promise<Response> | Observable<Response>;
-export type MangedFunctionExecutor<Payload = any> = { success: boolean, payload?: Payload, error?: { name: string, message: string } };
+export type MangedFunctionExecutor<Payload = any> = { success: boolean, payload?: Payload, error?: { name: string, message: string; }; };
