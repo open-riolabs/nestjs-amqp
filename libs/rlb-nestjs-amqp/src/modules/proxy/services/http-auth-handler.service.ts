c2ef0e731f8d74bd011d2d5400cc32c111673f16
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2025-05-02 12:53:37 +0200

    Refactor authentication configuration and handling to support multiple auth providers and improve JWT processing logic

--- libs/rlb-nestjs-amqp/src/modules/proxy/services/http-auth-handler.service.ts
+++ libs/rlb-nestjs-amqp/src/modules/proxy/services/http-auth-handler.service.ts
@@ -4,48 +4,107 @@ import { PathDefinition } from '@sicilyaction/lib-nestjs-core';
 import { HandlerAuthConfig } from '../../broker/config/handler-auth.config';
 import { Request, Response } from 'express';
 import { JwtService } from './jwt.service';
+import { ProcessedAuthData } from '..';
 
 @Injectable()
 export class HttpAuthHandlerService {
   constructor(
     private readonly jwtService: JwtService,
     private readonly configService: ConfigService) {
-    this.authConfig = this.configService.get<HandlerAuthConfig>('auth');
+    this.authProviders = this.configService.get<HandlerAuthConfig[]>("auth-providers");
   }
 
-  private readonly authConfig: HandlerAuthConfig;
+  private readonly authProviders: HandlerAuthConfig[];
 
-  async processAuthData(req: Request, path: PathDefinition): Promise<{ [key: string]: any; }> {
+  async processAuthData(req: Request, path: PathDefinition): Promise<ProcessedAuthData> {
+
+    let out: ProcessedAuthData = { success: false };
+    if (!path?.auth) return out;
+    const authConfig = this.authProviders.find(o => o.name === path.auth);
+    if (!authConfig) throw new Error(`Auth provider ${path.auth} not found`);
+
+    switch (authConfig.type) {
+      case 'basic': out = await this.checkBasicAuth(req, authConfig); break;
+      case 'jwt': out = await this.checkJwt(req, authConfig); break;
+      case 'jwks': out = await this.checkJwt(req, authConfig); break;
+      case 'str-compare': await this.checkStringCompare(req, authConfig); break;
+      default:
+        break;
+    }
+  }
+
+  async checkJwt(req: Request, authConfig: HandlerAuthConfig) {
     let decoded: any;
-    let out: { [key: string]: any; } = {};
+    let out: ProcessedAuthData = { success: false };
     const jwt = req.headers.authorization?.split(" ")[1];
-    if (this.authConfig.jwtSignMode === 'secret') {
-      decoded = await this.jwtService.verifyTokenSecret(jwt);
+    if (!jwt) return out; 
+    if (authConfig.type === 'jwt') {
+      decoded = await this.jwtService.verifyTokenSecret(authConfig, jwt);
     }
-    if (this.authConfig.jwtSignMode === 'jwks') {
-      decoded = await this.jwtService.verifyTokenJwks(jwt);
+    if (authConfig.type === 'jwks') {
+      decoded = await this.jwtService.verifyTokenJwks(authConfig, jwt);
     }
-    if (!decoded && path.auth) {
-      return undefined;
+    if (!decoded) {
+      return out;
     } else {
-      if (!this.authConfig.jwtMap) {
+      if (!authConfig.jwtMap) {
         out = decoded;
+        out.success = true;
       } else {
-        this.authConfig.jwtMap.map(o => o.split(':')).forEach(([source, dest]) => {
+        authConfig.jwtMap.map(o => o.split(':')).forEach(([source, dest]) => {
           if (decoded?.[source])
-            out[`${this.authConfig.headerPrefix}${dest.trim().toUpperCase()}`] = decoded?.[source];
+            out[`${authConfig.headerPrefix}${dest.trim().toUpperCase()}`] = decoded?.[source];
         });
+        out.success = true;
       }
       return out;
     }
+  }
+
+  async checkBasicAuth(req: Request, authConfig: HandlerAuthConfig) {
+    let out: ProcessedAuthData = { success: false };
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Basic ')) {
+      return out;
+    }
+
+    const base64Credentials = authHeader.split(' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+    const [username, password] = credentials.split(':');
+
+    if (username === authConfig.clientId && password === authConfig.clientSecret) {
+      out[`${authConfig.headerPrefix}USERNAME`] = username;
+      out.success = true;
+      return out;
+    }
+
+    return out;
+  }
+
+  async checkStringCompare(req: Request, authConfig: HandlerAuthConfig) {
+    let out: ProcessedAuthData = { success: false };
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith(authConfig.headerPrefix)) {
+      return out;
+    }
+
+    const token = authHeader.substring(authConfig.headerPrefix.length).trim();
+    if (token === authConfig.secret) {
+      out[`${authConfig.headerPrefix}TOKEN`] = token;
+      out.success = true;
+      return out;
+    }
+
+    return out;
   }
 
   checkRoles(data: { [key: string]: any; }, path: PathDefinition): boolean {
     if (!path?.auth) return true;
     if (!path?.roles) return true;
     if (!data) return false;
-
-    const roles: string[] = data[this.authConfig.rolesClaim];
+    const authConfig = this.authProviders.find(o => o.name === path.auth);
+    if (!authConfig) throw new Error(`Auth provider ${path.auth} not found`);
+    const roles: string[] = data[authConfig.rolesClaim];
     if (!roles) return false;
     if (path.roles.all && path.roles.some) throw new Error("Path definition can't have both 'all' and 'some' roles");
     if (path.roles.all) return path.roles.all.every((role: string) => roles.includes(role));
