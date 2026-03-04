c5a865effcc02c7b0893df6f1caefeb5cba146fc
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2026-03-04 09:21:50 +0100

    refactor: remove unused AmqpModule and clean up ProxyModule imports

--- libs/rlb-nestjs-amqp/src/modules/proxy/proxy.module.ts
+++ libs/rlb-nestjs-amqp/src/modules/proxy/proxy.module.ts
@@ -1,7 +1,6 @@
 import { HttpModule } from '@nestjs/axios';
 import { DynamicModule, Global, Module, Provider } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { BrokerModule } from '../broker';
 import { HttpAuthHandlerService } from './services/http-auth-handler.service';
 import { HttpHandlerService } from './services/http-handler.service';
 import { JwtService } from './services/jwt.service';
@@ -10,7 +9,7 @@ import { WebSocketService } from './services/websocket.service';
 
 @Global()
 @Module({
-  imports: [ConfigModule, BrokerModule, HttpModule],
+  imports: [ConfigModule, HttpModule],
   providers: [HttpHandlerService, JwtService, HttpAuthHandlerService, WebSocketService],
   exports: [HttpHandlerService]
 })
